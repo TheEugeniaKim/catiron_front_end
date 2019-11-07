2194ee1cc3c7b76c65d4768dfe27688fe5e5b93c
github username <github email address>
2019-11-07 14:13:42 -0500

    file setup

---
+++ src/components/Meow.js
@@ -0,0 +1,10 @@
+import React from 'react'
+
+const Meow = () => {
+
+    return (
+        <h2> Meow</h2>
+    )
+}
+
+export default Meow
---
+++ src/components/NavBar.js
@@ -0,0 +1,10 @@
+import React from 'react'
+
+const NavBar = () => {
+
+    return (
+        <h2>NavBar </h2>
+    )
+}
+
+export default NavBar
